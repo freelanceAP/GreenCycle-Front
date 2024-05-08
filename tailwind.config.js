74889b5c705b11ac2827552369ddf575eff506c4
Ken Castro <kacp.castro08@gmail.com>
2024-05-08 07:12:07 -0500

    final login

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slideUp: {
+          '0%': {
+            transform: 'translateY(100%)',
+            opacity: '0',
+          },
+          '100%': {
+            transform: 'translateY(0)',
+            opacity: '1',
+          },
+        },
+      },
+      animation: {
+        slideUp: 'slideUp 5s ease-out', 
+      },
+    },
   },
   plugins: [],
-}
+};
+
 
