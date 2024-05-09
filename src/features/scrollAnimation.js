d860f7454ec7dc2844c247282930490b2ad593b2
Ken Castro <kacp.castro08@gmail.com>
2024-05-09 18:05:06 -0500

    new animations for landing page

--- src/features/scrollAnimation.js
+++ src/features/scrollAnimation.js
@@ -1,13 +1,19 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const links = document.querySelectorAll('a[href^="#"]');
-    links.forEach(link => {
-        link.addEventListener('click', function (e) {
-            e.preventDefault();
-            const targetId = this.getAttribute('href').slice(1);
-            const targetElement = document.getElementById(targetId);
-            if (targetElement) {
-                targetElement.scrollIntoView({ behavior: 'smooth' });
-            }
-        });
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll('a[href^="#"]');
+  links.forEach((link) => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute("href").slice(1);
+      const targetElement = document.getElementById(targetId);
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: "smooth" });
+      }
     });
+  });
+});
+
+AOS.init({
+  duration: 1000, // Duración de la animación 
+  easing: "ease-in-out", // Efecto de suavizado
+  once: true,
 });
