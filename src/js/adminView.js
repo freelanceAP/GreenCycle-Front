8aba1e15d2863ad44f0cb2312641675acc7790ff
Alexander Pérez <83444111+alexander3130@users.noreply.github.com>
2024-05-19 11:57:27 -0500

    Se actualiza adinView js para que reciba los datos del servidor propio

--- src/js/adminView.js
+++ src/js/adminView.js
@@ -3,9 +3,8 @@ const tbody = document.querySelector("#lastRequests")
 console.log(tbody)
 
 //  Eventos
-document.addEventListener("DOMContentLoaded", () => {
-    getRequests()
-})
+getRequests()
+
 
 //  Funciones
 async function getDataMonths() {
@@ -93,29 +92,27 @@ async function getDataMonths() {
     return data;
 }
 async function getRequests() {
-    const respose = await fetch(`https://66483c022bb946cf2f9feebd.mockapi.io/api/v1/adminView_requests`)
-    const data = await respose.json()
-
-    console.log(data)
-    renderLastRequests(data);
+    const response = await fetch(`https://greencycle-back-production.up.railway.app/api/v1/request`);
+    const data = await response.json();
+    
+    console.log(data);
+    renderLastRequests(data.content);
 }
 
 function renderLastRequests(requests) {
-
-    // tbody.innerHTML = ""
-    cleanTbody()
+    cleanTbody();
 
     requests.forEach((request) => {
         tbody.innerHTML += `
             <tr class="bg-gray-900 border-b border-gray-800 hover:bg-gray-800">
                 <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-normal break-words dark:text-white">
-                    <img class="w-10 h-10 rounded-full" src="${request.avatar}">
+                    <img class="w-10 h-10 rounded-full" src="https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1223.jpg"> <!-- Reemplaza con la ruta correcta si tienes imágenes de avatar -->
                     <div class="ps-3">
-                        <div class="text-base font-semibold">${request.name}</div>
+                        <div class="text-base font-semibold">${request.user.userName}</div>
                         <div class="font-normal text-gray-500">${request.description}</div>
                     </div>
                 </th>
-                <td class="px-6 py-4">${request.date}</td>
+                <td class="px-6 py-4">${new Date(request.dateTime).toLocaleString()}</td>
                 <td class="px-6 py-4">
                     <button data-modal-toggle="admin-modal-${request.id}" data-modal-target="admin-modal-${request.id}" class="font-medium text-green-500 hover:underline">Admin</button>
                 </td>
@@ -129,21 +126,12 @@ function renderLastRequests(requests) {
                     <div class="relative bg-gray-900 rounded-lg shadow flex h-[80vh] items-stretch">
                         <!-- Left side content -->
                         <div class="w-1/2 p-4 md:p-5 border-r border-gray-600 flex flex-col gap-y-7 overflow-hidden mt-4">
-                            <h1 class="text-3xl font-semibold text-white ml-7">
-                                Request Details
-                            </h1>
-                            <p class="text-sm text-gray-500 ml-7">
-                                ${request.description}
-                            </p>
-                            <p class="text-sm text-gray-500 ml-7">
-                                ${request.name} - Client
-                            </p>
-                            <p class="text-sm text-gray-500 ml-7">
-                                <span class="font-semibold text-white">KG Totales de la Recolección:</span> ${request.totalKg} KG
-                            </p>
+                            <h1 class="text-3xl font-semibold text-white ml-7">Request Details</h1>
+                            <p class="text-sm text-gray-500 ml-7">${request.description}</p>
+                            <p class="text-sm text-gray-500 ml-7">${request.user.userName} - Client</p>
+                            <p class="text-sm text-gray-500 ml-7"><span class="font-semibold text-white">KG Totales de la Recolección:</span> ${request.quantityUnit} KG</p>
                             <div class="flex justify-center overflow-hidden">
-                                <img src="/public/assets/img/trash_truck_resource.png" alt="Collection img"
-                                    class="object-cover w-[45vh]">
+                                <img src="/public/assets/img/trash_truck_resource.png" alt="Collection img" class="object-cover w-[45vh]">
                             </div>
                         </div>
 
@@ -151,67 +139,26 @@ function renderLastRequests(requests) {
                         <div class="w-[50vh] flex flex-col justify-start ml-10 z-50 h-full mt-4">
                             <!-- Modal header -->
                             <div class="flex items-center justify-between p-4 md:p-5 relative">
-                                <h3 class="font-semibold text-3xl text-white">
-                                    Collection Status
-                                </h3>
+                                <h3 class="font-semibold text-3xl text-white">Collection Status</h3>
                             </div>
                             <!-- Modal body -->
                             <div class="p-4 md:p-5 flex-grow">
                                 <ol class="relative ms-3.5 mb-4 md:mb-5">
                                     <li class="mb-10 ms-8">
-                                        <button
-                                            class="absolute time-line flex items-center justify-center w-6 h-6 bg-gray-400 rounded-full -start-3.5 ring-8 ring-gray-400 hover:bg-gray-500 transition text-white">
+                                        <button class="absolute time-line flex items-center justify-center w-6 h-6 bg-gray-400 rounded-full -start-3.5 ring-8 ring-gray-400 hover:bg-gray-500 transition text-white">
                                             <ion-icon name="time"></ion-icon>
                                         </button>
-                                        <h3 class="flex items-start mb-1 text-lg font-semibold text-gray-400">
-                                            Pending
-                                        </h3>
-                                        <time class="block mb-2 text-sm font-normal leading-none text-gray-400 ">Released
-                                            on January 13th, 2022</time>
+                                        <h3 class="flex items-start mb-1 text-lg font-semibold text-gray-400">Pending</h3>
+                                        <time class="block mb-2 text-sm font-normal leading-none text-gray-400 ">Released on ${new Date(request.dateTime).toLocaleString()}</time>
                                     </li>
-                                    <li class="mb-10 ms-8">
-                                        <button
-                                            class="absolute flex items-center justify-center w-6 h-6 bg-gray-400 rounded-full -start-3.5 ring-8 ring-gray-400 hover:bg-gray-500 transition text-white">
-                                            <ion-icon name="cube"></ion-icon>
-                                        </button>
-                                        <h3 class="flex items-start mb-1 text-lg font-semibold text-gray-400">
-                                            In Collection
-                                        </h3>
-                                        <time class="block mb-2 text-sm font-normal leading-none text-gray-400 ">Released
-                                            on January 13th, 2022</time>
-                                    </li>
-                                    <li class="mb-10 ms-8">
-                                        <button
-                                            class="absolute flex items-center justify-center w-6 h-6 bg-green-500 rounded-full -start-3.5 ring-8 ring-green-500 hover:bg-green-600 transition text-white">
-                                            <ion-icon name="sync"></ion-icon>
-                                        </button>
-                                        <h3 class="flex items-start mb-1 text-lg font-semibold text-green-500">
-                                            In Progress
-                                        </h3>
-                                        <time class="block mb-2 text-sm font-normal leading-none text-white">Released
-                                            on January 13th, 2022</time>
-                                    </li>
-                                    <li class="mb-10  ms-8">
-                                        <button
-                                            class="absolute flex items-center justify-center w-6 h-6 bg-gray-400 rounded-full -start-3.5 ring-8 ring-gray-400 hover:bg-gray-500 transition text-white">
-                                            <ion-icon name="checkmark-done"></ion-icon>
-                                        </button>
-                                        <h3 class="flex items-start mb-1 text-lg font-semibold text-gray-400">
-                                            Completed
-                                        </h3>
-                                        <time class="block mb-2 text-sm font-normal text-gray-400">Released
-                                            on January 13th, 2022</time>
-                                    </li>
+                                    <!-- Puedes agregar más estados según lo necesites -->
                                 </ol>
-                                <button
-                                    class="z-50 text-white inline-flex w-full justify-center bg-green-500 mb-7 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                                <button class="z-50 text-white inline-flex w-full justify-center bg-green-500 mb-7 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
                                     Download Certificate
                                 </button>
                             </div>
                         </div>
-                        <button type="button"
-                            class="text-gray-400 bg-transparent hover:text-white rounded-lg text-3xl h-8 w-8 inline-flex justify-center items-center absolute right-4 top-4"
-                            data-modal-toggle="admin-modal-${request.id}" id="closeModalBtn">
+                        <button type="button" class="text-gray-400 bg-transparent hover:text-white rounded-lg text-3xl h-8 w-8 inline-flex justify-center items-center absolute right-4 top-4" data-modal-toggle="admin-modal-${request.id}" id="closeModalBtn">
                             <ion-icon name="close-outline"></ion-icon>
                             <span class="sr-only">Close modal</span>
                         </button>
@@ -220,6 +167,8 @@ function renderLastRequests(requests) {
             </div>
         `;
     });
+
+    
 }
 
 function cleanTbody() {
