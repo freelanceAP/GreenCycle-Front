a10ed5b86614bfe1ee8a47ff2c5a6e07b5138034
Alexander Pérez <83444111+alexander3130@users.noreply.github.com>
2024-05-20 13:57:03 -0500

    se soluciona error create

--- src/js/clientsView.js
+++ src/js/clientsView.js
@@ -1,45 +1,37 @@
 //  Importo el URLbase para no hacer tan largas las URLS y poder cambiar desde solo una parte la URL
 import { URLbase } from './app.js';
 
+const form =  document.querySelector('#userForm')
+
 
 //   Conjunto de selectores para que funcione el formulario de create
 document.addEventListener('DOMContentLoaded', function() {
+    
     // Aquí va tu código JavaScript
     document.getElementById('userForm').addEventListener('submit', async function(event) {
         event.preventDefault(); // Previene el comportamiento por defecto del formulario (recargar la página)
+        
+        const formData = new FormData(form)
 
-        userName = document.getElementById('userName').value;
-        const email = document.getElementById('email').value;
-        const password = document.getElementById('password').value;
-        const role = document.getElementById('role').value;
-        const nit = document.getElementById('nit').value;
-        const address = document.getElementById('address').value;
-        const phone = document.getElementById('phone').value;
-        const description = document.getElementById('description').value;
-        const target = document.getElementById('target').value;
-        const targetProgress = document.getElementById('targetProgress').value;
+        console.log(formData)
+        console.log("holaaa")
 
-        const data = {
-            userName: userName,
-            email,
-            password,
-            role,
-            nit: parseInt(nit),
-            address,
-            phone: parseInt(phone),
-            description,
-            target,
-            targetProgress
-        };
-        console.log(document.getElementById('userName'))
+        const objForm = {}
+
+        for (const [key, value] of formData) {
+            objForm[key] = value;
+          }
+
+
+      
         try {
-            console.log(data)
+          
             const response = await fetch('https://greencycle-back-production.up.railway.app/api/v1/users', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify(data)
+                body: JSON.stringify(objForm)
             });
 
             if (response.ok) {
